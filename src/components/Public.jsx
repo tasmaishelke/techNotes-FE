592f8abddb886c21fcb96098d1df2ac50a20e960
tasmai <tasmaishelke@gmail.com>
2024-07-19 11:30:56 +0530

    final commit

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
-const Public = () =>
-{
+const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Tasz's Technotes</span></h1>
+                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
             </header>
             <main className="public__main">
                 <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
@@ -22,8 +21,8 @@ const Public = () =>
                 <Link to="/login">Employee Login</Link>
             </footer>
         </section>
+
     )
     return content
 }
-
 export default Public
--- src/features/users/NewUserForm.jsx
+++ src/features/users/NewUserForm.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSave } from "@fortawesome/free-solid-svg-icons"
 import { ROLES } from "../../config/roles"
 
-const USER_REGEX = /^[A-z-1-9]{3,20}$/
+const USER_REGEX = /^[A-z-0-9]{3,20}$/
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
 
 const NewUserForm = () => {
